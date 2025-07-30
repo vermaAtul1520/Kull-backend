b237a1ab42eb981e531069593b67a3d7c3a7b4cb
parmanand007 <parmanandprajapati006@gmail.com>
2025-07-30 23:26:59 +0530

    feat(likes&comments)

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -11,4 +11,14 @@ router.post("/", isAuthenticated, postController.createPost);
 router.put("/:id", isAuthenticated, postController.updatePost);
 router.delete("/:id", isAuthenticated, postController.deletePost);
 
+// Toggle like/unlike
+router.post('/likes/:postId', isAuthenticated, postController.toggleLike)
+// Get likes for a post
+router.get('/likes/:postId', postController.getPostLikes);
+
+// Comments
+router.post('/comments/:postId', isAuthenticated, postController.createComment);
+router.get('/comments/:postId', postController.getComments);
+router.delete('/comments/:commentId', isAuthenticated, postController.deleteComment);
+
 module.exports = router;
